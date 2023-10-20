823a41ae6aa5bc934de299f860f459a47aab5527
Raghuvaran725 <kraghuvaran7097@gmail.com>
2023-10-20 10:52:54 +0530

    Controller class commit

---
+++ src/main/java/com/cg/controller/SupplierController.java
@@ -0,0 +1,34 @@
+package com.cg.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.SupplierEntity;
+import com.cg.service.SupplierService;
+
+@RestController
+@RequestMapping("/supplierapi")
+public class SupplierController {
+	
+	@Autowired
+	private SupplierService service;
+	
+	@PostMapping("/")
+	public String  addProduct(@RequestBody SupplierEntity s) {
+		return service.addSupplier(s);
+	}
+	
+	@GetMapping("/")
+	public List<SupplierEntity> getAll(@RequestParam("id") int id){
+		return service.getSupplierList();
+	}
+	
+
+}
